b604213bfc02a33f3191495aafa435673d2b6d77
Harshit Kumar <kharshit020@gmail.com>
2024-09-10 18:57:23 +0530

    Implemented login and logut

---
+++ my-app/src/Logout.jsx
@@ -0,0 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+import react from "react"
+
+const LogoutButton = () => {
+    const {logout} = useAuth0();
+
+
+    return (
+        <button onClick={() => logout({logoutParams: { returnTo : window.location.origin} })}>Logout</button>
+    )
+}
+
+export default LogoutButton
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.jsx'
 import './index.css'
+import LoginButton from './Login.jsx'
+import LogoutButton from './Logout.jsx'
+
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 createRoot(document.getElementById('root')).render(
 
@@ -16,6 +21,9 @@ createRoot(document.getElementById('root')).render(
          }}>
 
     <App />
+    <LoginButton />
+    <LogoutButton />
   </Auth0Provider>
+
   </>
 )
